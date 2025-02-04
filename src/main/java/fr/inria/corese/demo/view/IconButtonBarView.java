92bb8163ea95565bf428c2e892c9a74d4693747d
Chloé Vandenbrulle <115253350+ChloeVandenbrulle@users.noreply.github.com>
2025-02-04 16:55:30 +0100

    update view IconButton

--- src/main/java/fr/inria/corese/demo/view/IconButtonBarView.java
+++ src/main/java/fr/inria/corese/demo/view/IconButtonBarView.java
@@ -3,7 +3,10 @@ package fr.inria.corese.demo.view;
 import fr.inria.corese.demo.enums.IconButtonType;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.util.Duration;
 import org.kordamp.ikonli.javafx.FontIcon;
 import org.kordamp.ikonli.Ikon;
 import org.kordamp.ikonli.materialdesign2.*;
@@ -33,17 +36,17 @@ public class IconButtonBarView extends VBox {
 
     private Button createButton(IconButtonType type) {
         return switch (type) {
-            case SAVE -> createIconButton(MaterialDesignC.CONTENT_SAVE_OUTLINE);
-            case OPEN_FILE -> createIconButton(MaterialDesignF.FOLDER_OPEN);
-            case EXPORT -> createIconButton(MaterialDesignE.EXPORT);
-            case IMPORT -> createIconButton(MaterialDesignI.IMPORT);
-            case CLEAR -> createIconButton(MaterialDesignB.BROOM);
-            case UNDO -> createIconButton(MaterialDesignU.UNDO);
-            case REDO -> createIconButton(MaterialDesignR.REDO);
-            case DOCUMENTATION -> createIconButton(MaterialDesignB.BOOK_OPEN_VARIANT);
-            case ZOOM_IN -> createIconButton(MaterialDesignM.MAGNIFY_PLUS_OUTLINE);
-            case ZOOM_OUT -> createIconButton(MaterialDesignM.MAGNIFY_MINUS_OUTLINE);
-            case FULL_SCREEN -> createIconButton(MaterialDesignF.FULLSCREEN);
+            case SAVE -> createIconButton(MaterialDesignC.CONTENT_SAVE_OUTLINE, "Save");
+            case OPEN_FILE -> createIconButton(MaterialDesignF.FOLDER_OPEN, "Open file");
+            case EXPORT -> createIconButton(MaterialDesignE.EXPORT, "Export");
+            case IMPORT -> createIconButton(MaterialDesignI.IMPORT, "Import");
+            case CLEAR -> createIconButton(MaterialDesignB.BROOM, "Clear");
+            case UNDO -> createIconButton(MaterialDesignU.UNDO, "Undo");
+            case REDO -> createIconButton(MaterialDesignR.REDO, "Redo");
+            case DOCUMENTATION -> createIconButton(MaterialDesignB.BOOK_OPEN_VARIANT, "Documentation");
+            case ZOOM_IN -> createIconButton(MaterialDesignM.MAGNIFY_PLUS_OUTLINE, "Zoom in");
+            case ZOOM_OUT -> createIconButton(MaterialDesignM.MAGNIFY_MINUS_OUTLINE, "Zoom out");
+            case FULL_SCREEN -> createIconButton(MaterialDesignF.FULLSCREEN, "Full screen");
         };
     }
 
@@ -51,34 +54,41 @@ public class IconButtonBarView extends VBox {
         return buttons.get(type);
     }
 
-    private Button createIconButton(Ikon icon) {
+    private Button createIconButton(Ikon icon, String tooltipText) {
         FontIcon fontIcon = new FontIcon(icon);
-        fontIcon.setIconSize(20);  // Taille de l'icône en pixels
+        fontIcon.setIconSize(22);  // Taille de l'icône en pixels
         Button button = new Button();
         button.setGraphic(fontIcon);
         button.setStyle(
-            "-fx-background-color: white;" +
-            "-fx-border-radius: 20;" +
-            "-fx-background-radius: 20;" +
-            "-fx-min-height: 10;" +
-            "-fx-max-width: 10;"
+            "-fx-background-color: transparent;" +
+            "-fx-pref-width: 6;" +
+            "-fx-pref-height: 6;"
         );
 
-        button.setOnMouseEntered(e -> button.setStyle(
-            "-fx-background-color: #E3F2FD;" +
-            "-fx-border-radius: 20;" +
-            "-fx-background-radius: 20;" +
-            "-fx-min-height: 10;" +
-            "-fx-max-width: 10;"
-        ));
+        Tooltip tooltip = new Tooltip(tooltipText);
 
-        button.setOnMouseExited(e -> button.setStyle(
-            "-fx-background-color: white;" +
-            "-fx-border-radius: 20;" +
-            "-fx-background-radius: 20;" +
-            "-fx-min-height: 10;" +
-            "-fx-max-width: 10;"
-        ));
+        Tooltip.install(button, tooltip);
+
+        button.setOnMouseEntered(e -> {
+            button.setStyle(
+                    "-fx-background-color: #2196F3;"+
+                    "-fx-pref-width: 6;" +
+                    "-fx-pref-height: 6;"
+            );
+            fontIcon.setIconColor(Color.WHITE);
+            tooltip.setStyle(
+                    "-fx-font-size: 14;"
+            );
+        });
+
+        button.setOnMouseExited(e -> {
+            button.setStyle(
+                    "-fx-background-color: transparent;" +
+                    "-fx-pref-width: 6;" +
+                    "-fx-pref-height: 6;"
+            );
+            fontIcon.setIconColor(Color.BLACK);
+        });
         return button;
     }
 
